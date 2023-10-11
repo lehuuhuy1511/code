7a560220f66c1a1ba729a30136b610c1a6b7fa22
lehuuhuy1511 <146902635+lehuuhuy1511@users.noreply.github.com>
2023-10-11 10:07:33 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,47 +1,19 @@
-// const fs = require ('fs');
+const express = require('express');
 
-// let dataFromFile = fs.readFileSync('./package.json','utf-8');
-// console.log(dataFromFile);
+let app = express();
 
-// fs.readFile('./package.json', ' utf-8', (error,data) =>{
-//     if(error){ console.log(error); }
-//     consolog(data);
-// });
+app.use(express.static('pulic'));
 
-// let dataWriteFile = "hello,it's write file!";
-
-// let dataObjectWriteFile = {
-//     a:5,
-//     b:6
-// }
-// fs.writeFile('test.txt',JSON.stringify (dataWriteFile),(err)=>{
-//     if(err){console.log(err)}
-//     console.log("Write file success!");
-// });
-// fs.readFile('test.txt', ' utf-8', (error,data) =>{
-//     if(error){ console.log(error); }
-//     console.log(Object.keys (JSON.parse(data)));
-// });
-
-// let fileSystem = require ('./fileController');
-
-// console.log (fileSystem)
-
-// fileSystem . readFile('test.txt', (fileData) =>{
-// console.log(FileData)
-// });
-// console.log(fileSystem.readFileSync ("haisdh"))
-
-// const express = require ('express');
-
-// let app = express();
-//     app.get('/',(rep,res)=>{
-//         res.send("Hello it's ***hub");
-//     });
-
-// app.listen(6969, (err)=>{
-//     if(err){ console.log(err);}
-//     console.log("app is start at port 6969");
-// });
-
-
+app.get('/', (req, res)=>{
+    res.sendFileS(path.realove(__dirname + '/public/index.html'));
+});
+app.get('/gift', (req,res)=>{
+    res.send("9GB lonk :http://**************************");
+});
+app.get('/about', (req,res)=>{
+    res.send("This is about");
+});
+app.listen(6969, (err)=>{
+    if(err) {console.log(err);}
+    console.log("App is start at port 6969");
+});
